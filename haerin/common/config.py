d3ca26d78288b2eb0ef085d67e7b848db42518c9
KSE <szn3700@gamil.com>
2024-11-07 14:01:40 +0900

    remove others files from teammates

--- haerin/common/config.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.agents import AgentFinish
-
-def should_continue(data):
-    """Checks if the agent outcome indicates the end of the conversation."""
-    if isinstance(data['agent_outcome'], AgentFinish):
-        return "end"
-    else:
-        return "continue"
